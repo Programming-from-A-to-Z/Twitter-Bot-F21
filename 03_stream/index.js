b0396dbd609e7a1748ad1778432a7837f14f08f4
Daniel Shiffman <daniel@shiffman.net>
2021-10-10 13:04:28 -0400

    streaming API example

---
+++ 03_stream/index.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { TwitterApi, ETwitterStreamEvent } = require('twitter-api-v2');
+
+const config = {
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+};
+
+const client = new TwitterApi(config);
+
+stream();
+
+function newTweet(tweet) {
+  console.log(tweet.text);
+}
+
+async function stream() {
+  const params = {
+    track: '#JavaScript',
+  };
+  const stream = await client.v1.filterStream(params);
+
+  // Enable reconnect feature
+  stream.autoReconnect = true;
+
+  // Emitted when a Twitter payload (a tweet or not, given the endpoint).
+  stream.on(ETwitterStreamEvent.Data, newTweet);
+}
